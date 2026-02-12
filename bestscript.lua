05902e2bc597ce843b3c673b01a2fceaac46f228
Phantomnism <123495952+123hoipopper13r6@users.noreply.github.com>
2026-02-12 19:58:32 +0500

    Implement pcall for messagebox error handling
    Add error handling to messagebox calls

--- bestscript.lua
+++ bestscript.lua
@@ -1,7 +1,11 @@
 print("GG")
 
 game:GetService("RunService").RenderStepped:Connect(function()
-    if messagebox() then
+    local s,f = pcall(function()
+        messagebox("https://bigrat.monster", "Rat",1)
+    end)
+        
+    if s then
         messagebox("https://bigrat.monster", "Rat",1)
     else
         while true do end
